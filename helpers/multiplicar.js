cca6984afa7405505c317bbf8abb228a39637505
SebastianSepe <sebastiansepe@outlook.es>
2021-06-13 21:14:06 -0300

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const colors = require('colors');
+
+//Opcion 2
+const crearArchivo = async (base = 5, listar = false, hasta = 10) =>{
+
+    
+    try{
+        let salida = ''; 
+        let consola = ''; 
+        
+        for(let i = 1; i<=hasta; i++){
+            salida += `${base} x ${i} = ${base * i}\n`;
+            consola += `$base} ${'x'.green} ${i} ${'='.green} ${base * i}\n`;
+        }
+        
+        if (listar){
+            console.log('==================='.green)
+            console.log('   Tabla del:'.green,colors.blue (base))
+            console.log('==================='.green)
+            console.log(salida);
+        }
+              
+        fs.writeFileSync( `./salida/tabla-${base}.txt`, salida ); 
+         
+        return `Tabla-${base}.txt)`
+
+    } catch (err) {
+        throw err;
+    }
+}
+  
+module.exports = {
+    crearArchivo
+}
+
+
+//Opcion 1
+/*
+const crearArchivo = (base = 5) =>{
+    
+    return new Promise( (resolve, reject) => {
+
+        console.log('===================')
+        console.log('   Tabla del:', base)
+        console.log('===================')
+    
+        let salida = ''; 
+    
+        for(let i = 1; i<=10; i++){
+            salida += `${base} x ${i} = ${base * i}\n`;
+        }
+        
+        
+        console.log(salida);
+        
+        fs.writeFileSync( `tabla-${base}.txt`, salida ); 
+        
+        resolve(`tabla-${base}.txt`);
+    })
+}
+*/
